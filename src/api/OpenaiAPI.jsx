63a1948b68994a2680c2b18478be8f40054c5a88
airfly626 <aiirlyme@gmail.com>
2025-08-21 23:07:28 +0800

    Update OpenaiAPI.jsx

--- src/api/OpenaiAPI.jsx
+++ src/api/OpenaiAPI.jsx
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 
 const client = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
 });
 
 export const sendTextMessageToOpenAI = async (inputText) => {
